867510e19449ab27cb464daf8842696ed10f125d
shunsuke <wos.shun@gmail.com>
2021-05-01 23:27:05 +0900

    first commit

---
+++ resources/views/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row justify-content-center ml-0 mr-0 h-100">
+    <div class="card w-100">
+        <div class="card-header">新規メモ作成</div>
+        <div class="card-body">
+            <form method='POST' action="../public/store" enctype="multipart/form-data">
+                @csrf
+                <input type='hidden' name='user_id' value="{{ $user['id'] }}">
+                <div class="form-group">
+                     <textarea name='content' class="form-control"rows="10"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="tag">タグ</label>
+                    <input name='tag' type="text" class="form-control" id="tag" placeholder="タグを入力">
+                </div>
+                <div class="form-group">
+                    <label for="tag">画像登録</label>
+                    <input type='file' name="image" class="form-control-file" id="image">
+                </div>
+                <button type='submit' class="btn btn-primary btn-lg">保存</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
